eed512f41d2ed441f87f9b9d94c389520b34d9d8
Fernando Giardina <fgiardina@beatmobile.com.ar>
2020-09-27 12:01:13 -0300

    .

--- capacitacion/variables/index.js
+++ capacitacion/variables/index.js
@@ -1,1 +1,16 @@
 // Variables
+
+// las variables en javascript pueden declararse como:
+// var o let: su contenido puede ser modificado
+// const: es constante y su contenido no puede modificarse en el transcurso del programa
+
+var nombre = "Jose";
+var apellido = "Gomez";
+
+console.log(nombre, apellido);
+
+nombre = "Miguel";
+console.log(nombre, apellido);
+
+const direccion = "San Martin 1234";
+console.log(direccion);
