b79a5367e901f98b558821b92e5ce3c08f2c71e2
marianogiardina <marianogiardina1@gmail.com>
2020-09-28 20:13:57 -0300

    arreglue el if

--- capacitacion/condicionales/index.js
+++ capacitacion/condicionales/index.js
@@ -4,7 +4,7 @@ var nro1 = 10
 var nro2 = 20
 
 if (nro1 > nro2) {
-    console.log('el nro 1 es menor al nro 2')
-} else {
     console.log('el nro 1 es mayor al nro 2')
+} else {
+    console.log('el nro 1 es menor al nro 2')
 }
