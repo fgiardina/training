063686a4f735fe55c8cdd6af358b69442034924c
marianogiardina <marianogiardina1@gmail.com>
2020-09-28 20:30:08 -0300

    interpolacion variables

--- capacitacion/condicionales/index.js
+++ capacitacion/condicionales/index.js
@@ -1,12 +1,12 @@
 // Condicionales
 
-var nro1 = 10
+var nro1 = 40
 var nro2 = 20
 
 if (nro1 > nro2) {
-    console.log('el nro 1 es mayor al nro 2')
+    console.log(`el ${nro1} es mayor al ${nro2}`) //para que el mensaje muestre el contenido de la variable se tiene que usar las comillas inclinadas
 } else {
-    console.log('el nro 1 es menor al nro 2')
+    console.log(`el ${nro1} es menor al ${nro2}`)
 }
 
 // mostrar en cada mensaje a que nros nos estamos refiriendo
